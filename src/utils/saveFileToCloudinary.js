4c5180748e66e38605e81ebde56c0ecd2dc48969
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2026-06-17 20:36:41 +0300

    -corrctPhoto

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -15,28 +15,37 @@ cloudinary.config({
   api_secret,
 });
 
-export const saveFileToCloudinary = async (file, folder) => {
-  // console.log('file', file);
-  // console.log('folder', folder);
+// export const saveFileToCloudinary = async (file, folder) => {
+//   // console.log('file', file);
+//   // console.log('folder', folder);
 
+//   try {
+//   const response = await cloudinary.uploader.upload(file.path, {folder}); // завантаження файлів в cloudnary
+//   // console.log('response', response.url);  //  інформація про завантажений файл
+//     return response.url;
+//   }
+//   catch(error) {
+//     // throw error;
+//   }
+//   finally {
+//     await unlink(file.path); // видалення файлу temp
+//   }
+// };
+
+
+export const saveFileToCloudinary = async (file, folder) => {
   try {
-  // const response = await cloudinary.uploader.upload(file.path, {folder}); // завантаження файлів в cloudnary
-    // console.log('response', response.url);  //  інформація про завантажений файл
-    const response = await cloudinary.uploader
-      .upload_stream({ folder }, (err, result) => {
-        if (err) console.log(err);
-        else console.log(result.url);
-      })
-      .end(file.buffer);
-    return response.url;
-  }
-  catch(error) {
-    // throw error;
-  }
-  finally {
-    await unlink(file.path); // видалення файлу temp
+    const result = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream({ folder }, (err, result) => {
+          if (err) return reject(err);
+          resolve(result);
+        })
+        .end(file.buffer);
+    });
+
+    return result.url;
+  } catch (error) {
+    console.log(error);
   }
 };
-
-
-
